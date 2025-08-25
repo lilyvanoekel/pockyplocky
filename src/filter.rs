bf17c751c5ff7931cf200d327f4b2bfd2732cd0e
Lily van Oekel <lilyvanoekel@gmail.com>
2025-08-25 17:33:18 -0400

    add proper resonator

--- src/filter.rs
+++ src/filter.rs
@@ -52,27 +52,19 @@ impl ResonantFilter {
         self.a2 = a2 / a0;
     }
 
-    // pub fn set_frequency(&mut self, frequency: f32, q: f32) {
-    //     let w0 = 2.0 * std::f32::consts::PI * frequency / self.sample_rate;
-    //     let cos_w0 = w0.cos();
-    //     let sin_w0 = w0.sin();
+    pub fn set_mode(&mut self, frequency: f32, decay: f32) {
+        let w = 2.0 * std::f32::consts::PI * frequency / self.sample_rate;
+        let r = (-1.0 / (decay * self.sample_rate)).exp();
 
-    //     let alpha = sin_w0 / (2.0 * q);
+        let a1 = -2.0 * r * w.cos();
+        let a2 = r * r;
 
-    //     // Constant peak gain form
-    //     let b0 = q * alpha;
-    //     let b1 = 0.0;
-    //     let b2 = -q * alpha;
-    //     let a0 = 1.0 + alpha;
-    //     let a1 = -2.0 * cos_w0;
-    //     let a2 = 1.0 - alpha;
-
-    //     self.b0 = b0 / a0;
-    //     self.b1 = b1 / a0;
-    //     self.b2 = b2 / a0;
-    //     self.a1 = a1 / a0;
-    //     self.a2 = a2 / a0;
-    // }
+        self.b0 = 1.0;
+        self.b1 = 0.0;
+        self.b2 = 0.0;
+        self.a1 = a1;
+        self.a2 = a2;
+    }
 
     pub fn process(&mut self, input: f32) -> f32 {
         let y = self.b0 * input + self.b1 * self.x1 + self.b2 * self.x2
@@ -98,9 +90,62 @@ impl ResonantFilter {
     }
 }
 
+pub struct ModalResonator {
+    b0: f32,
+    a1: f32,
+    a2: f32,
+    y1: f32,
+    y2: f32,
+    sample_rate: f32,
+}
+
+impl ModalResonator {
+    pub fn new(sample_rate: f32) -> Self {
+        Self {
+            b0: 0.0,
+            a1: 0.0,
+            a2: 0.0,
+            y1: 0.0,
+            y2: 0.0,
+            sample_rate,
+        }
+    }
+
+    pub fn set_mode(&mut self, frequency: f32, t60: f32) {
+        if frequency <= 0.0 || t60 <= 0.0 {
+            self.b0 = 0.0;
+            self.a1 = 0.0;
+            self.a2 = 0.0;
+            return;
+        }
+
+        let omega = 2.0 * std::f32::consts::PI * frequency / self.sample_rate;
+        let r = (-6.91 / (t60 * self.sample_rate)).exp();
+        self.a1 = -2.0 * r * omega.cos();
+        self.a2 = r * r;
+        self.b0 = frequency / self.sample_rate;
+    }
+
+    pub fn process(&mut self, input: f32) -> f32 {
+        let y = self.b0 * input - self.a1 * self.y1 - self.a2 * self.y2;
+        self.y2 = self.y1;
+        self.y1 = y;
+        y
+    }
+
+    pub fn reset(&mut self) {
+        self.y1 = 0.0;
+        self.y2 = 0.0;
+    }
+
+    pub fn set_sample_rate(&mut self, sr: f32) {
+        self.sample_rate = sr;
+    }
+}
+
 // Modal synthesis using multiple resonant filters
 pub struct ModalFilter {
-    filters: [ResonantFilter; 4], // 4 modal filters
+    filters: [ModalResonator; 4], // 4 modal filters
     amplitudes: [f32; 4],         // Amplitude for each mode
     sample_rate: f32,
 }
@@ -108,7 +153,7 @@ pub struct ModalFilter {
 impl ModalFilter {
     pub fn new(sample_rate: f32) -> Self {
         Self {
-            filters: std::array::from_fn(|_| ResonantFilter::new(sample_rate)),
+            filters: std::array::from_fn(|_| ModalResonator::new(sample_rate)),
             amplitudes: [1.0, 0.5, 0.3, 0.2], // Default modal amplitudes
             sample_rate,
         }
@@ -126,31 +171,10 @@ impl ModalFilter {
             // Disable modes above 20kHz or set amplitude to 0
             if modal_freq > 20000.0 {
                 self.amplitudes[i] = 0.0;
-                filter.set_frequency(20000.0, q);
+                filter.set_mode(20000.0, 1.0);
             } else {
                 self.amplitudes[i] = amplitudes[i];
-                filter.set_frequency(modal_freq, q);
-            }
-        }
-    }
-
-    pub fn set_physics_frequency(
-        &mut self,
-        fundamental_freq: f32,
-        q: f32,
-        modes: &[crate::physics::Mode; 4],
-    ) {
-        for (i, (filter, mode)) in self.filters.iter_mut().zip(modes.iter()).enumerate() {
-            let modal_freq = mode.freq as f32;
-            filter.set_sample_rate(self.sample_rate);
-
-            // Disable modes above 20kHz or set amplitude to 0
-            if modal_freq > 20000.0 {
-                self.amplitudes[i] = 0.0;
-                filter.set_frequency(20000.0, q);
-            } else {
-                self.amplitudes[i] = mode.amp as f32;
-                filter.set_frequency(modal_freq, q);
+                filter.set_mode(modal_freq, 1.0);
             }
         }
     }
